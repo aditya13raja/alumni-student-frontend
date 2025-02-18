b8decebba3d7c3e39eb5f56d7e390713619584d5
aditya13raja <aditya13raja@gmail.com>
2025-02-18 19:35:22 +0530

    Create basic routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 function App() {
 
   return (
-    <>
-        <h1 className="text-3xl">App working</h1>
-    </>
+    <BrowserRouter>
+        <Header />
+        
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile/>} />
+            <Route path="/signin" element={<SignIn/>} />
+            <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+
+        <Footer />
+    </BrowserRouter>
   ) 
 }
 
