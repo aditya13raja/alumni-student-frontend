c0b894e47df86df68071fb64bafa0cf1994c358b
aditya13raja <aditya13raja@gmail.com>
2025-03-19 09:29:49 +0530

    Change default target backend route to the deployed route of the backend

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,10 @@ export default defineConfig({
         proxy: {
             // add target url for posting and fetching data from backend
             '/api' : {
-                target: 'http://127.0.0.1:1234',
-                secure: false,
+                //target: 'http://127.0.0.1:1234',
+                target: 'https://alumni-student-backend.onrender.com',
+                changeOrigin: true,
+                secure: true,
             }
         }
     }
